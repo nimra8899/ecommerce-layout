741e324bb0d8c61dfb82bb8c9e323044c89e4f48
Nimra Jabbar <105984076+nimra8899@users.noreply.github.com>
2024-09-12 13:22:17 -0700

    Add files via upload

---
+++ e.js
@@ -0,0 +1,18 @@
+let mainimg=document.getElementById("main-img");
+let small_img=document.getElementsByClassName("small-img");
+small_img[0].onclick=function(){
+    mainimg.src=small_img[0].src;//small img par click krne se ye big imag or small img ko exchenge krdega ga
+
+}
+small_img[1].onclick=function(){
+    mainimg.src=small_img[1].src;//small img par click krne se ye big imag or small img ko exchenge krdega ga
+    
+}
+small_img[2].onclick=function(){
+    mainimg.src=small_img[2].src;//small img par click krne se ye big imag or small img ko exchenge krdega ga
+    
+}
+small_img[3].onclick=function(){
+    mainimg.src=small_img[3].src;//small img par click krne se ye big imag or small img ko exchenge krdega ga
+    
+}
